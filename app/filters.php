9a9c159b26b7363c8c1f07a151ccbeceb24ef137
raverona <raverona@hotmail.com>
2016-05-23 10:27:31 -0300

    adição de permissões para cors

--- app/filters.php
+++ app/filters.php
@@ -30,6 +30,8 @@ App::before(function($request)
 	});
 
 	header('Access-Control-Allow-Origin: *'); 
+	header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+	header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 });
 
