b635238d40f2468de8f6376700a54d9e3685ed51
raverona <raverona@hotmail.com>
2016-06-15 09:23:21 -0300

    retornando nome do usuário junto as fotos do feed

--- app/lib/api/controllers/APIFeedController.php
+++ app/lib/api/controllers/APIFeedController.php
@@ -15,7 +15,7 @@ class APIFeedController extends \BaseController {
 		return \Response::json(\User::join('friendship as fs', 'fs.following_id', '=', 'users.id')
 			->join('users as f', 'f.id', '=', 'fs.followed_id')
 			->join('photos as p', 'p.user_id', '=', 'f.id')
-			->select('f.id as user_id', 'f.photo as avatar', 'p.id as photo_id', 'p.name', 'p.nome_arquivo')
+			->select('f.id as user_id', 'f.name as user_name', 'f.photo as avatar', 'p.id as photo_id', 'p.name', 'p.nome_arquivo')
 			->where('users.id', '=', $id)
 			->whereNull('p.deleted_at')
 			->orderBy('p.updated_at', 'desc')->take(20)->get());
@@ -28,7 +28,7 @@ class APIFeedController extends \BaseController {
 		return \Response::json(\User::join('friendship as fs', 'fs.following_id', '=', 'users.id')
 			->join('users as f', 'f.id', '=', 'fs.followed_id')
 			->join('photos as p', 'p.user_id', '=', 'f.id')
-			->select('f.id as user_id', 'f.photo as avatar', 'p.id as photo_id', 'p.name', 'p.nome_arquivo')
+			->select('f.id as user_id', 'f.name as user_name', 'f.photo as avatar', 'p.id as photo_id', 'p.name', 'p.nome_arquivo')
 			->where('users.id', '=', $id)
 			->where('p.id', '<', $max_id)
 			->whereNull('p.deleted_at')
