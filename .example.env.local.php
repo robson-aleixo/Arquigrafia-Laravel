ba899979aaef06fc5ab876e66dfe34eb65ca2694
rockrick1 <henriquecerquinhp@gmail.com>
2019-10-08 16:09:29 -0300

    Update example env file with instructions

--- .example.env.local.php
+++ .example.env.local.php
@@ -2,8 +2,8 @@
 //file: /.env.local.php
 // return the configuration for the 'local' environment
 return array(
-    'db_localhost' => 'localhost',
-    'db_host'      => '127.0.0.1',
+    'db_localhost' => 'db', // use db container name to use with docker.
+    'db_host'      => 'db', // else, use localhost and 127.0.0.1 dor local use
     'db_name'      => 'arquigrafia', // specify database name
     'db_user'      => 'username', // specify database username
     'db_pass'      => '', // specify database password
