2a472cfd4d6f83ee81d6f8d19a649a539c4b4219
rockrick1 <henriquecerquinhp@gmail.com>
2019-10-08 16:03:37 -0300

    Add files to docker branch
    Docker containers working with docker-compose
    
    env variables working (variables on .env.local.php, there is an example file included)

---
+++ .example.env.local.php
@@ -0,0 +1,10 @@
+<?php
+//file: /.env.local.php
+// return the configuration for the 'local' environment
+return array(
+    'db_localhost' => 'localhost',
+    'db_host'      => '127.0.0.1',
+    'db_name'      => 'arquigrafia', // specify database name
+    'db_user'      => 'username', // specify database username
+    'db_pass'      => '', // specify database password
+);
