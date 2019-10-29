d59f6ce53281f7e554defb8a534f9f0f2f248505
Gabriel Massukado <gabriel.massukado@usp.br>
2019-10-28 22:11:52 -0300

    add tag tables

--- app/modules/collaborative/views/tag/index.blade.php
+++ app/modules/collaborative/views/tag/index.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.default')
 
+<head>
+    <title>Relatório de Tags</title>
+    <meta charset="UTF-8">
+  </head>
+
     @section('content')
         <h1> TAGS </h1>
-        <a href="/tags/create">Criar nova tag</a href>
+        <a href="/tags/create">Clique aqui para criar uma nova tag de acervo</a href>
+        <hr>
+        <br>
+        <div class="container">
+        <div class="twelve columns">
         <h2>Tags de Acervo</h2>
+        <div style="height:500px;width:500px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
         <table style="width:100%">
         @if(count($tags_a) > 0)
             @foreach($tags_a as $tag)
@@ -25,7 +35,10 @@
             <p>Não há tags de acervo para mostrar.</p>
         @endif
     </table>
+    </div>
+    <br>
     <h2>Tags de Usuário</h2>
+    <div style="height:500px;width:500px;border:1px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
     <table style="width:100%">
             @if(count($tags_u) > 0)
                 @foreach($tags_u as $tag)
@@ -36,6 +49,9 @@
             @else
                 <p>Não há tags de usuário para mostrar.</p>
             @endif
-        </table>
+    </table>
+    </div>
+    </div>
+    </div>
     @endsection
 </html>
