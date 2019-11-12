49409794f580606817c7bc27342aee1df3774eba
Gabriel Massukado <gabriel.massukado@usp.br>
2019-11-12 14:55:37 -0300

    adds reports page

---
+++ app/views/admin_reports.blade.php
@@ -0,0 +1,21 @@
+<html>
+@extends('layouts.default')
+<head>
+    <title>Relatórios</title>
+     <meta charset="UTF-8">
+</head>
+@section('content')
+    <div class="news clearfix">
+        <hgroup class="profile_block_title">
+            <h1><img src="{{ asset("img/logo-mini.png") }}" width="16" height="16"/>
+            Relatórios de administrador:
+            </h1>
+        </hgroup>
+        <div class="container">
+            <ul style="list-style-type:square">
+                <li><a href="/tags">Relatório de Tags</a href></li>
+                <li><a href="/tombos">Relatório de Tombos</a href></li>
+            <ul>
+        </div>
+    </div>
+@endsection
