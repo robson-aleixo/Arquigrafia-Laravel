1226f231cf34c777ef2eba145f6011cc96f5133c
Gabriel Massukado <gabriel.massukado@usp.br>
2019-11-15 15:49:12 -0300

    fixes view

--- app/views/admin_reports.blade.php
+++ app/views/admin_reports.blade.php
@@ -5,17 +5,19 @@
      <meta charset="UTF-8">
 </head>
 @section('content')
-    <div class="news clearfix">
-        <hgroup class="profile_block_title">
-            <h1><img src="{{ asset("img/logo-mini.png") }}" width="16" height="16"/>
-            Relatórios de administrador:
-            </h1>
-        </hgroup>
-        <div class="container">
-            <ul style="list-style-type:square">
-                <li><a href="/tags">Relatório de Tags</a href></li>
-                <li><a href="/tombos">Relatório de Tombos</a href></li>
-            <ul>
+    <div class='container'>
+        <div class="columns">
+            <hgroup class="profile_block_title">
+                <h1><img src="{{ asset("img/logo-mini.png") }}" width="16" height="16"/>
+                Relatórios de administrador:
+                </h1>
+            </hgroup>
+            <div class="container">
+                <ul style="list-style-type:square">
+                    <li><a href="/tags">Relatório de Tags</a href></li>
+                    <li><a href="/tombos">Relatório de Tombos</a href></li>
+                <ul>
+            </div>
         </div>
     </div>
 @endsection
