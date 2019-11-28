ef4a7397d8187dd7376198e59f32baa9411dd477
rockrick1 <henriquecerquinho@gmail.com>
2019-11-28 17:13:10 -0300

    Add env for convenience

---
+++ .env.local.php
@@ -0,0 +1,10 @@
+<?php
+//file: /.env.local.php
+// return the configuration for the 'local' environment
+return array(
+    'db_localhost' => 'db',
+    'db_host'      => 'db',
+    'db_name'      => 'arquigrafia', // specify database name
+    'db_user'      => 'root', // specify database username
+    'db_pass'      => '', // specify database password
+);
