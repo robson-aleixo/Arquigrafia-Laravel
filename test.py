7a604b784fd778bd6489d41fbde3e87431799f49
Robson Aleixo <robson.aleixo@outlook.com.br>
2019-12-02 15:30:35 -0300

    testando sem acesso de usuário

--- test.py
+++ test.py
@@ -1,23 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
-
-# This is the first test to be run
-
-# driver = webdriver.Chrome()
-driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.CHROME)
-   
+driver = webdriver.Firefox(executable_path=os.getcwd() + "/geckodriver")
 driver.get('http://localhost:8000')
 element = driver.find_element_by_xpath("/html/body/div/div[4]/div[2]/div/div/div/div/a[2]/div")
-element.click()
-name = driver.find_element_by_xpath('//*[@id="login"]')
-name.send_keys("local2")
-password = driver.find_element_by_xpath('//*[@id="password"]')
-password.send_keys("local2")
-login = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/form/p[3]/input')
-login.click()
-report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
 assert report is not None
+# element.click()
+# name = driver.find_element_by_xpath('//*[@id="login"]')
+# name.send_keys("robson")
+# password = driver.find_element_by_xpath('//*[@id="password"]')
+# password.send_keys("123456")
+# login = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/form/p[3]/input')
+# login.click()
+# report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
+# assert report is not None
 print("Uau, vc é um admin")
