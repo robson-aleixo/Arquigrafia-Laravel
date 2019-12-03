c0b28c9dca6a335e052eec8d303dab539d07d041
rockrick1 <henriquecerquinho@gmail.com>
2019-12-03 14:08:10 -0300

    naosei

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 # This is the first test to be run
 
-# driver = webdriver.Chrome()
-driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.CHROME)
+driver = webdriver.Firefox()
+# driver = webdriver.Remote(
+#    command_executor='http://127.0.0.1:4444/wd/hub',
+#    desired_capabilities=DesiredCapabilities.CHROME)
    
 driver.get('http://localhost:8000')
 element = driver.find_element_by_xpath("/html/body/div/div[4]/div[2]/div/div/div/div/a[2]/div")
 element.click()
 name = driver.find_element_by_xpath('//*[@id="login"]')
-name.send_keys("local2")
+name.send_keys("rockrick")
 password = driver.find_element_by_xpath('//*[@id="password"]')
-password.send_keys("local2")
+password.send_keys("potato")
 login = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/form/p[3]/input')
 login.click()
 report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
