987bdba4f582dde156ec89eab6d36c027d63bd46
Robson Aleixo <robson.aleixo@outlook.com.br>
2019-12-03 19:28:41 -0300

    testando ...

--- .env.local.php
+++ .env.local.php
@@ -1,18 +1,18 @@
 <?php
 //file: /.env.local.php
 // return the configuration for the 'local' environment
-return array(
-    'db_localhost' => 'db', // use db container name to use with docker.
-    'db_host'      => 'db', // else, use localhost and 127.0.0.1 dor local use
-    'db_name'      => 'arquigrafia', // specify database name
-    'db_user'      => 'root', // specify database username
-    'db_pass'      => '', // specify database password
-);
-
 // return array(
-//     'db_localhost' => 'localhost', // use db container name to use with docker.
-//     'db_host'      => '127.0.0.1', // else, use localhost and 127.0.0.1 dor local use
+//     'db_localhost' => 'db', // use db container name to use with docker.
+//     'db_host'      => 'db', // else, use localhost and 127.0.0.1 dor local use
 //     'db_name'      => 'arquigrafia', // specify database name
 //     'db_user'      => 'root', // specify database username
-//     'db_pass'      => '@Rpa1988', // specify database password
+//     'db_pass'      => '', // specify database password
 // );
+
+return array(
+    'db_localhost' => 'localhost', // use db container name to use with docker.
+    'db_host'      => '127.0.0.1', // else, use localhost and 127.0.0.1 dor local use
+    'db_name'      => 'arquigrafia', // specify database name
+    'db_user'      => 'root', // specify database username
+    'db_pass'      => '@Rpa1988', // specify database password
+);
