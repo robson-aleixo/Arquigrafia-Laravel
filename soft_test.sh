68110aa0dc7e5953101ca6411b18f05ada68276f
Gabriel Massukado <gabriel.massukado@usp.br>
2019-12-05 10:56:42 -0300

    script para rodar testes sem inicializar o bd

--- soft_test.sh
+++ soft_test.sh
@@ -5,6 +5,10 @@ DOCKER="-dk"
 
 if [ FLAG == DOCKER ]; then
     python3 test_admin.py -dk
+    python3 test_tags.py -dk
+    python3 test_tombos.py -dk
 else
     python3 test_admin.py
+    python3 test_tags.py
+    python3 test_tombos.py
 fi
