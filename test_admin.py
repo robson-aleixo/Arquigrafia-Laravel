dd83f68ca9a2bc8dcff7f34ad26efdc48609a0a8
Gabriel Massukado <gabriel.massukado@usp.br>
2019-12-05 09:20:45 -0300

    dysfunctional admin test

--- test_admin.py
+++ test_admin.py
@@ -2,13 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import sys
+import os
 
-def test1(host):
-    ### Test 1 - Test if the user can log in as an admin ###
-    global passes
-    result = False
 
-    driver = webdriver.Firefox()
+## If you add more tests, change this number
+total = 2
+passes = 0
+
+flag = ''
+if len(sys.argv) > 1:
+    flag = str(sys.argv[1])
+host  = 'http://localhost:8000/'
+if flag == '-dk':
+    host  = 'http://localhost:8009/'
+
+
+### Test 1 - Test if the user can log in as an admin ###
+try:
+    driver = webdriver.Firefox(executable_path=os.getcwd() + "/geckodriver")
     driver.get(host)
     login_button = driver.find_element_by_class_name("login-inicio")
     login_button.click()
@@ -19,25 +30,18 @@ def test1(host):
     password.send_keys("teste1")
     password.send_keys(Keys.ENTER)
     driver.forward()
-
-    try:
-        print("ENTREI NO TRY")
-        report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
-        print("PASSEI PELA LINHA")
-    except:
-        print("ENTREI NO EXCEPT")
-        report = None
+    report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
     
     if report is not None: #The report button should only appear when the user is an admin 
-        result = True
         passes += 1
-    return result
+        print("Teste de login como admin concluuído com sucesso")
 
-def test2(host):
-    ### Test 2 - Test if the user can log in while not being an admin an cannot access admin pages ###
-    global passes
-    result = False
+except Exception as ex:
+	print("ERRO no teste de login como admin: " + str(ex))
 
+
+### Test 2 - Test if the user can log in while not being an admin and cannot access admin pages ###
+try:
     driver = webdriver.Firefox()
     driver.get(host)
     login_button = driver.find_element_by_class_name("login-inicio")
@@ -51,10 +55,7 @@ def test2(host):
     driver.forward()
     time.sleep(1)
 
-    try:
-        report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
-    except:
-        report = None
+    report = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[6]/a')
     
     driver.get(host + 'adm-reports')
     driver.forward()
@@ -63,25 +64,12 @@ def test2(host):
 
     #The report button should only appear when the user is an admin and they should have been redirected 
     if (report is None) and (url == (host + 'home')):
-        result = True
         passes += 1
-    return result
+        print("Teste de login como não-admin concluuído com sucesso")
 
-## If you add more tests, change this number
-total = 2
-passes = 0
+except Exception as ex:
+	print("ERRO no teste de login não-admin: " + str(ex))
 
-flag = ''
-if len(sys.argv) > 1:
-    flag = str(sys.argv[1])
-host  = 'http://localhost:8000/'
-if flag == '-dk':
-    host  = 'http://localhost:8009/'
 
-print("Starting admin tests...")
-if not test1(host):
-    print("Test 1 failed!")
-if not test2(host):
-    print("Test 2 failed!")
 print("%i/%i tests passed" % (passes, total))
 
