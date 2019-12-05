cbe52fbc25e6cbf7bbe214d76520fcc8a1627dbe
Gabriel Massukado <gabriel.massukado@usp.br>
2019-12-05 17:35:21 -0300

    add script for running tests without setup

--- soft_test.sh
+++ soft_test.sh
@@ -7,8 +7,10 @@ if [ FLAG == DOCKER ]; then
     python3 test_admin.py -dk
     python3 test_tags.py -dk
     python3 test_tombos.py -dk
+    python3 test_instituitons.py -dk
 else
     python3 test_admin.py
     python3 test_tags.py
     python3 test_tombos.py
+    python3 test_instituitons.py
 fi
